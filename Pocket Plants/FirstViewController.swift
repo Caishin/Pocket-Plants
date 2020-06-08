3e1253ef087bc636447b1221e220b5485fe1edff
Cameron Aishin <cameron.aishin@gmail.com>
2020-06-07 22:31:55 -0700

    Initial Commit

---
+++ Pocket Plants/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Pocket Plants
+//
+//  Created by Cameron Aishin on 6/7/20.
+//  Copyright © 2020 Cameron Aishin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pocket Plants/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Pocket Plants
+//
+//  Created by Cameron Aishin on 6/7/20.
+//  Copyright © 2020 Cameron Aishin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
